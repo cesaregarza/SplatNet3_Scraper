b9db527509950c86a7d503b62078496d9db6464a
Cesar E Garza <cesar.garza.826@gmail.com>
2023-01-31 01:45:27 -0600

    test__get_detailed_query

--- tests/scraper/test_main.py
+++ tests/scraper/test_main.py
@@ -6,7 +6,7 @@ import requests
 
 from splatnet3_scraper.base.tokens import TokenManager
 from splatnet3_scraper.scraper.config import Config
-from splatnet3_scraper.scraper.main import SplatNet3_Scraper
+from splatnet3_scraper.scraper.main import SplatNet3_Scraper, QueryMap
 from tests.mock import MockConfig, MockResponse, MockTokenManager
 
 config_path = "splatnet3_scraper.scraper.config.Config"
@@ -62,4 +62,71 @@ class TestSplatNet3Scraper:
             with pytest.raises(KeyError):
                 scraper._SplatNet3_Scraper__get_query("test_url")
             assert mock_get.call_count == 2
+            mock_generate.assert_called_once()
+
+    def test__get_detailed_query(self):
+        scraper = SplatNet3_Scraper(MockConfig())
+
+        # versus True 200 response
+        with patch.object(scraper, "_SplatNet3_Scraper__query") as mock_get:
+            json = {"data": {"test_key": "test_value"}}
+            mock_get.return_value = MockResponse(200, json=json)
+            assert (
+                scraper._SplatNet3_Scraper__get_detailed_query("test_game_id")
+                == json["data"]
+            )
+            expected_variables = {"vsResultId": "test_game_id"}
+            mock_get.assert_called_once_with(
+                QueryMap.VS_DETAIL, variables=expected_variables
+            )
+
+        # versus True not 200 response
+        with (
+            patch.object(scraper, "_SplatNet3_Scraper__query") as mock_get,
+            patch.object(
+                MockTokenManager, "generate_all_tokens"
+            ) as mock_generate,
+        ):
+            mock_get.return_value = MockResponse(404)
+            with pytest.raises(KeyError):
+                scraper._SplatNet3_Scraper__get_detailed_query("test_game_id")
+            assert mock_get.call_count == 2
+            expected_variables = {"vsResultId": "test_game_id"}
+            mock_get.assert_called_with(
+                QueryMap.VS_DETAIL, variables=expected_variables
+            )
+            mock_generate.assert_called_once()
+
+        # versus False 200 response
+        with patch.object(scraper, "_SplatNet3_Scraper__query") as mock_get:
+            json = {"data": {"test_key": "test_value"}}
+            mock_get.return_value = MockResponse(200, json=json)
+            assert (
+                scraper._SplatNet3_Scraper__get_detailed_query(
+                    "test_game_id", versus=False
+                )
+                == json["data"]
+            )
+            expected_variables = {"coopHistoryDetailId": "test_game_id"}
+            mock_get.assert_called_once_with(
+                QueryMap.SALMON_DETAIL, variables=expected_variables
+            )
+
+        # versus False not 200 response
+        with (
+            patch.object(scraper, "_SplatNet3_Scraper__query") as mock_get,
+            patch.object(
+                MockTokenManager, "generate_all_tokens"
+            ) as mock_generate,
+        ):
+            mock_get.return_value = MockResponse(404)
+            with pytest.raises(KeyError):
+                scraper._SplatNet3_Scraper__get_detailed_query(
+                    "test_game_id", versus=False
+                )
+            assert mock_get.call_count == 2
+            expected_variables = {"coopHistoryDetailId": "test_game_id"}
+            mock_get.assert_called_with(
+                QueryMap.SALMON_DETAIL, variables=expected_variables
+            )
             mock_generate.assert_called_once()
