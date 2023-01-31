9ba1db1e7548c25b46a749d1e01e7fe568ec5657
Cesar E Garza <cesar.garza.826@gmail.com>
2023-01-31 02:05:59 -0600

    test_get_mode_summary

--- tests/scraper/test_main.py
+++ tests/scraper/test_main.py
@@ -6,10 +6,12 @@ import requests
 
 from splatnet3_scraper.base.tokens import TokenManager
 from splatnet3_scraper.scraper.config import Config
-from splatnet3_scraper.scraper.main import SplatNet3_Scraper, QueryMap
+from splatnet3_scraper.scraper.main import QueryMap, SplatNet3_Scraper
+from splatnet3_scraper.scraper.responses import QueryResponse
 from tests.mock import MockConfig, MockResponse, MockTokenManager
 
 config_path = "splatnet3_scraper.scraper.config.Config"
+query_response_path = "splatnet3_scraper.scraper.responses.QueryResponse"
 
 
 class TestQueryMap:
@@ -21,6 +23,7 @@ class TestQueryMap:
         # Invalid
         with pytest.raises(AttributeError):
             QueryMap.get("invalid")
+
 
 class TestSplatNet3Scraper:
     def test_from_config_file(
@@ -139,3 +142,43 @@ class TestSplatNet3Scraper:
                 QueryMap.SALMON_DETAIL, variables=expected_variables
             )
             mock_generate.assert_called_once()
+
+    def test_get_mode_summary(self):
+        scraper = SplatNet3_Scraper(MockConfig())
+
+        # Invalid mode
+        with pytest.raises(ValueError):
+            scraper.get_mode_summary("invalid")
+
+        # Valid mode, not detailed
+        with (
+            patch.object(scraper, "_SplatNet3_Scraper__get_query") as mock_get,
+            patch(query_response_path + ".__init__") as mock_query_response,
+        ):
+            mock_query_response.return_value = None
+            get_return_value = {"test_key": "test_value"}
+            mock_get.return_value = get_return_value
+
+            scraper.get_mode_summary("anarchy", detailed=False)
+            mock_get.assert_called_once_with(QueryMap.get("anarchy"))
+            mock_query_response.assert_called_once_with(
+                summary=get_return_value
+            )
+
+        # Valid mode, detailed
+        with (
+            patch.object(
+                scraper, "_SplatNet3_Scraper__vs_with_details"
+            ) as mock_get,
+            patch(query_response_path + ".__init__") as mock_query_response,
+        ):
+            mock_query_response.return_value = None
+            get_return_summary = {"test_key": "test_value"}
+            get_return_detailed = []
+            mock_get.return_value = (get_return_summary, get_return_detailed)
+
+            scraper.get_mode_summary("anarchy", detailed=True)
+            mock_get.assert_called_once_with(QueryMap.get("anarchy"), None)
+            mock_query_response.assert_called_once_with(
+                summary=get_return_summary, detailed=get_return_detailed
+            )
