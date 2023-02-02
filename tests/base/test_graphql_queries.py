c844e2b85bb752dc793d98ee62aed1ffd94230c1
Cesar E Garza <cesar.garza.826@gmail.com>
2023-02-02 01:22:45 -0600

    more graphql tests

--- tests/base/test_graphql_queries.py
+++ tests/base/test_graphql_queries.py
@@ -1,7 +1,17 @@
+from unittest.mock import patch
+
 import pytest
 import requests
 
 from splatnet3_scraper.base.graph_ql_queries import GraphQLQueries
+from splatnet3_scraper.constants import (
+    DEFAULT_USER_AGENT,
+    GRAPHQL_URL,
+    SPLATNET_URL,
+)
+
+utils_path = "splatnet3_scraper.utils"
+test_graphql_path = "splatnet3_scraper.base.graph_ql_queries.GraphQLQueries"
 
 
 class TestGraphQLQueries:
@@ -26,6 +36,78 @@ class TestGraphQLQueries:
         assert queries.hash_map == {"test": "test"}
         assert isinstance(queries.session, requests.Session)
 
-    # The other functions are so simple that they don't need to be tested, they
-    # generally are helper functions to generate dictionaries with specific keys
-    # and values.
+    def test_query_header(self):
+        queries = GraphQLQueries()
+        expected_bullet_token = "test_bullet_token"
+        expected_language = "test_language"
+        expected_splatnet_web_version = "test_version"
+        override = {"test_key": "test_value"}
+        expected_header = {
+            "Authorization": f"Bearer {expected_bullet_token}",
+            "Accept-Language": expected_language,
+            "User-Agent": DEFAULT_USER_AGENT,
+            "X-Web-View-Ver": expected_splatnet_web_version,
+            "Content-Type": "application/json",
+            "Accept": "*/*",
+            "Origin": SPLATNET_URL,
+            "X-Requested-With": "com.nintendo.znca",
+            "Referer": (
+                f"{SPLATNET_URL}?"
+                f"lang={expected_language}"
+                f"&na_country={expected_language[-2:]}"
+                f"&na_lang={expected_language}"
+            ),
+            "Accept-Encoding": "gzip, deflate",
+            "test_key": "test_value",
+        }
+        with patch(
+            utils_path + ".get_splatnet_web_version"
+        ) as mock_get_version:
+            mock_get_version.return_value = expected_splatnet_web_version
+            header = queries.query_header(
+                expected_bullet_token,
+                expected_language,
+                override=override,
+            )
+        assert header == expected_header
+
+    def test_query(self):
+        queries = GraphQLQueries()
+        query_name = "anarchy"
+        bullet_token = "test_bullet_token"
+        gtoken = "test_gtoken"
+        language = "test_language"
+        user_agent = "test_user_agent"
+
+        with (
+            patch(test_graphql_path + ".query_header") as mock_query_header,
+            patch(test_graphql_path + ".query_body") as mock_query_body,
+            patch("requests.Session.post") as mock_post,
+        ):
+            mock_query_header.return_value = "test_header"
+            mock_query_body.return_value = "test_body"
+            mock_post.return_value = "test_response"
+            response = queries.query(
+                query_name,
+                bullet_token,
+                gtoken,
+                language,
+                user_agent,
+            )
+        mock_query_header.assert_called_once_with(
+            bullet_token,
+            language,
+            user_agent,
+            {},
+        )
+        mock_query_body.assert_called_once_with(
+            query_name,
+            {},
+        )
+        mock_post.assert_called_once_with(
+            GRAPHQL_URL,
+            headers="test_header",
+            data="test_body",
+            cookies={"_gtoken": gtoken},
+        )
+        assert response == "test_response"
