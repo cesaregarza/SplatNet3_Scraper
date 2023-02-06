7d29715665d1856de801f7c01460468d24c2205e
Cesar E Garza <cesar.garza.826@gmail.com>
2023-02-06 00:12:58 -0600

    fix on the version for publishing

--- splatnet3_scraper/__init__.py
+++ splatnet3_scraper/__init__.py
@@ -1,13 +1,6 @@
 import pathlib
 import re
 
-# Read version number from TOML
-self_path = pathlib.Path(__file__).parent.parent
-toml_path = self_path / "pyproject.toml"
-with open(toml_path, "rb") as f:
-    lines = f.readlines()
-version_line = [str(line) for line in lines if "version" in str(line)][0]
-version = re.search(r"\d+\.\d+\.\d+", version_line).group(0)  # type: ignore
-__version__ = version
+__version__ = "0.4.1"
 
 from splatnet3_scraper.scraper import SplatNet3_Scraper
---
+++ tests/test_misc.py
@@ -0,0 +1,14 @@
+# This does not test the actual functionality of the code. Anything that needs
+# some sort of assurance before pushing to production should be tested here.
+import splatnet3_scraper
+import re
+
+
+def test_version():
+    # Make sure the version in the toml file matches the version in the code.
+    with open("pyproject.toml") as f:
+        lines = f.readlines()
+
+    version_line = [str(line) for line in lines if "version" in str(line)][0]
+    version = re.search(r"\d+\.\d+\.\d+", version_line).group(0)
+    assert version == splatnet3_scraper.__version__
