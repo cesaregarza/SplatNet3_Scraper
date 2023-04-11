f0706e5b87bc39bdd22529ad607da9f187b9d681
Cesar E Garza <cesar@cegarza.com>
2023-04-11 01:44:27 -0500

    removed xfail

--- tests/query/test_json_parser.py
+++ tests/query/test_json_parser.py
@@ -479,8 +479,3 @@ class TestJSONParser:
             )
             mock_load.assert_called_once()
             assert json_parser.data == data
-
-    # TODO: Figure out how to test from_parquet
-    @pytest.mark.xfail
-    def test_from_parquet(self):
-        raise NotImplementedError
