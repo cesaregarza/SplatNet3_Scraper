6ee01ca116c203b4703c8372b695707836b8b394
Cesar E Garza <cesar@cegarza.com>
2023-09-18 02:13:32 -0500

    started work on config refactor

--- splatnet3_scraper/query/config.py
+++ splatnet3_scraper/query/config.py
@@ -9,14 +9,130 @@ from splatnet3_scraper.auth import (
 from splatnet3_scraper.constants import DEFAULT_USER_AGENT, IMINK_URL
 
 
+class ConfigOptions:
+    _ACCEPTED_OPTIONS = [
+        "user_agent",
+        "log_level",
+        "log_file",
+        "export_path",
+        "language",
+        "lang",
+        "country",
+        "stat.ink_api_token",
+        "stat_ink_api_token",
+        "statink_api_token",
+        "f_token_url",
+    ]
+
+    _DEPRECATED_OPTIONS = {
+        "api_key": "stat.ink_api_token",
+        "f_gen": "f_token_url",
+        "ftoken_url": "f_token_url",
+    }
+
+    _DEFAULT_OPTIONS = {
+        "user_agent": DEFAULT_USER_AGENT,
+        "f_gen": IMINK_URL,
+        "export_path": "",
+        "language": "en-US",
+        "country": "US",
+    }
+
+    def __init__(self) -> None:
+        self.additional_accepted_options: list[str] = []
+        self.additional_deprecated_options: dict[str, str] = {}
+        self.additional_default_options: dict[str, str] = {}
+
+    @property
+    def ACCEPTED_OPTIONS(self) -> list[str]:
+        return self._ACCEPTED_OPTIONS + self.additional_accepted_options
+
+    @property
+    def DEPRECATED_OPTIONS(self) -> dict[str, str]:
+        return {
+            **self._DEPRECATED_OPTIONS,
+            **self.additional_deprecated_options,
+        }
+
+    @property
+    def DEFAULT_OPTIONS(self) -> dict[str, str]:
+        return {**self._DEFAULT_OPTIONS, **self.additional_default_options}
+
+    @property
+    def SUPPORTED_OPTIONS(self) -> list[str]:
+        return self.ACCEPTED_OPTIONS + list(self.DEPRECATED_OPTIONS.keys())
+
+    def add_accepted_options(self, options: list[str]) -> None:
+        """Add options to the list of accepted options.
+
+        Args:
+            options (list[str]): The list of options to add.
+        """
+        self.additional_accepted_options.extend(options)
+
+    def add_deprecated_options(self, options: dict[str, str]) -> None:
+        """Add options to the list of deprecated options.
+
+        Args:
+            options (dict[str, str]): The list of options to add.
+        """
+        self.additional_deprecated_options.update(options)
+
+    def add_default_options(self, options: dict[str, str]) -> None:
+        """Add options to the list of default options.
+
+        Args:
+            options (dict[str, str]): The list of options to add.
+        """
+        self.additional_default_options.update(options)
+
+    def remove_accepted_options(self, options: list[str]) -> None:
+        """Remove options from the list of accepted options.
+
+        Args:
+            options (list[str]): The list of options to remove.
+        """
+        for option in options:
+            if option in self.additional_accepted_options:
+                self.additional_accepted_options.remove(option)
+            else:
+                self._ACCEPTED_OPTIONS.remove(option)
+
+    def remove_deprecated_options(self, options: list[str]) -> None:
+        """Remove options from the list of deprecated options.
+
+        Args:
+            options (list[str]): The list of options to remove.
+        """
+        for option in options:
+            if option in self.additional_deprecated_options:
+                self.additional_deprecated_options.pop(option)
+            else:
+                self._DEPRECATED_OPTIONS.pop(option)
+
+    def remove_default_options(self, options: list[str]) -> None:
+        """Remove options from the list of default options.
+
+        Args:
+            options (list[str]): The list of options to remove.
+        """
+        for option in options:
+            if option in self.additional_default_options:
+                self.additional_default_options.pop(option)
+            else:
+                self._DEFAULT_OPTIONS.pop(option)
+
+
 class Config:
     """Class that can access the token manager as well as additional options."""
 
     def __init__(
         self,
         config_path: str | None = None,
-        *args,
+        *,
         token_manager: TokenManager | None = None,
+        write_to_file: bool = True,
+        config: configparser.ConfigParser | None = None,
     ) -> None:
         """Initializes the class. If token_manager is given, it will assume that
         this is a first time initialization and has not been setup yet.
@@ -33,22 +149,24 @@ class Config:
             config_path (str | None): The path to the config file. If None, it
                 will look for ".splatnet3_scraper" in the current working
                 directory. Defaults to None.
-            *args: These are ignored.
             token_manager (TokenManager | None): The token manager to use.
                 Keyword argument. If given, it will skip the post-init method.
                 Defaults to None.
+            write_to_file (bool): Whether or not to write the config to the
+                config file. Defaults to True.
+            config (configparser.ConfigParser | None): The config parser to use.
+                If None, it will create a new one. Defaults to None.
         """
-        self.additional_accepted_options: list[str] = []
-        self.additional_deprecated_options: dict[str, str] = {}
-        self.additional_default_options: dict[str, str] = {}
+        self.write_to_file = write_to_file
+        self.config_options = ConfigOptions()
 
         if token_manager is None:
             self.generate_token_manager(config_path)
             return
-        else:
-            self.config_path = config_path
 
+        self.config_path = config_path
         self.token_manager = token_manager
+        self.config = config
         self.initialize_options()
 
     def generate_token_manager(self, config_path: str | None = None) -> None:
@@ -89,7 +207,12 @@ class Config:
         except configparser.NoSectionError:
             self.config.add_section("options")
             self.options = self.config.options("options")
-        self.manage_options()
+
+        for option in self.options:
+            self.manage_option(option)
+
+        if not self.write_to_file:
+            return
 
         with open(config_path, "w") as configfile:
             self.config.write(configfile)
@@ -173,78 +296,36 @@ class Config:
         with open(path, "w") as configfile:
             self.config.write(configfile)
 
-    def manage_options(self) -> None:
+    def manage_option(self, option: str) -> None:
         """Manage the options in the config file.
 
         This function will move invalid options to the "unknown" section and
         move deprecated options to the "deprecated" section while replacing them
         with the new option name.
         """
-        for option in self.options:
-            if option not in (
-                self.ACCEPTED_OPTIONS + list(self.DEPRECATED_OPTIONS.keys())
-            ):
-                if not self.config.has_section("unknown"):
-                    self.config.add_section("unknown")
-                self.config["unknown"][option] = self.config["options"][option]
-                self.config.remove_option("options", option)
-            if option in self.DEPRECATED_OPTIONS:
-                deprecated_name = option
-                option_name = self.DEPRECATED_OPTIONS[option]
-                if not self.config.has_section("deprecated"):
-                    self.config.add_section("deprecated")
-                # Make a copy of the deprecated option in the deprecated section
-                # and then replace the deprecated option with the new option
-                self.config["deprecated"][deprecated_name] = self.config[
-                    "options"
-                ][deprecated_name]
-                self.config["options"][option_name] = self.config["options"][
-                    deprecated_name
-                ]
-                self.config.remove_option("options", option)
+        if option not in self.config_options.SUPPORTED_OPTIONS:
+            if not self.config.has_section("unknown"):
+                self.config.add_section("unknown")
+            self.config["unknown"][option] = self.config["options"][option]
+            self.config.remove_option("options", option)
+            return
 
-    _ACCEPTED_OPTIONS = [
-        "user_agent",
-        "log_level",
-        "log_file",
-        "export_path",
-        "language",
-        "lang",
-        "country",
-        "stat.ink_api_token",
-        "stat_ink_api_token",
-        "statink_api_token",
-        "f_token_url",
-    ]
+        if option not in self.config_options.DEPRECATED_OPTIONS:
+            return
 
-    _DEPRECATED_OPTIONS = {
-        "api_key": "stat.ink_api_token",
-        "f_gen": "f_token_url",
-        "ftoken_url": "f_token_url",
-    }
-
-    _DEFAULT_OPTIONS = {
-        "user_agent": DEFAULT_USER_AGENT,
-        "f_gen": IMINK_URL,
-        "export_path": "",
-        "language": "en-US",
-        "country": "US",
-    }
-
-    @property
-    def ACCEPTED_OPTIONS(self) -> list[str]:
-        return self._ACCEPTED_OPTIONS + self.additional_accepted_options
-
-    @property
-    def DEPRECATED_OPTIONS(self) -> dict[str, str]:
-        return {
-            **self._DEPRECATED_OPTIONS,
-            **self.additional_deprecated_options,
-        }
-
-    @property
-    def DEFAULT_OPTIONS(self) -> dict[str, str]:
-        return {**self._DEFAULT_OPTIONS, **self.additional_default_options}
+        deprecated_name = option
+        option_name = self.config_options.DEPRECATED_OPTIONS[option]
+        if not self.config.has_section("deprecated"):
+            self.config.add_section("deprecated")
+        # Make a copy of the deprecated option in the deprecated section
+        # and then replace the deprecated option with the new option
+        self.config["deprecated"][deprecated_name] = self.config["options"][
+            deprecated_name
+        ]
+        self.config["options"][option_name] = self.config["options"][
+            deprecated_name
+        ]
+        self.config.remove_option("options", option)
 
     def get(self, key: str) -> str:
         """Get the value of an option. If the option is not set, the default
@@ -260,15 +341,15 @@ class Config:
         Returns:
             str: The value of the option.
         """
-        if key in self.ACCEPTED_OPTIONS:
+        if key in self.config_options.ACCEPTED_OPTIONS:
             if key in self.config["options"]:
                 return self.config["options"][key]
-            elif key in self.DEFAULT_OPTIONS:
-                return self.DEFAULT_OPTIONS[key]
+            elif key in self.config_options.DEFAULT_OPTIONS:
+                return self.config_options.DEFAULT_OPTIONS[key]
             else:
                 raise KeyError(f"Option not set and has no default: {key}")
-        elif key in self.DEPRECATED_OPTIONS:
-            return self.get(self.DEPRECATED_OPTIONS[key])
+        elif key in self.config_options.DEPRECATED_OPTIONS:
+            return self.get(self.config_options.DEPRECATED_OPTIONS[key])
         else:
             raise KeyError(f"Invalid option: {key}")
 
@@ -311,63 +392,3 @@ class Config:
             str: The value of the token.
         """
         return self.token_manager.get(key, full_token)
-
-    def add_accepted_options(self, options: list[str]) -> None:
-        """Add options to the list of accepted options.
-
-        Args:
-            options (list[str]): The list of options to add.
-        """
-        self.additional_accepted_options.extend(options)
-
-    def add_deprecated_options(self, options: dict[str, str]) -> None:
-        """Add options to the list of deprecated options.
-
-        Args:
-            options (dict[str, str]): The list of options to add.
-        """
-        self.additional_deprecated_options.update(options)
-
-    def add_default_options(self, options: dict[str, str]) -> None:
-        """Add options to the list of default options.
-
-        Args:
-            options (dict[str, str]): The list of options to add.
-        """
-        self.additional_default_options.update(options)
-
-    def remove_accepted_options(self, options: list[str]) -> None:
-        """Remove options from the list of accepted options.
-
-        Args:
-            options (list[str]): The list of options to remove.
-        """
-        for option in options:
-            if option in self.additional_accepted_options:
-                self.additional_accepted_options.remove(option)
-            else:
-                self._ACCEPTED_OPTIONS.remove(option)
-
-    def remove_deprecated_options(self, options: list[str]) -> None:
-        """Remove options from the list of deprecated options.
-
-        Args:
-            options (list[str]): The list of options to remove.
-        """
-        for option in options:
-            if option in self.additional_deprecated_options:
-                self.additional_deprecated_options.pop(option)
-            else:
-                self._DEPRECATED_OPTIONS.pop(option)
-
-    def remove_default_options(self, options: list[str]) -> None:
-        """Remove options from the list of default options.
-
-        Args:
-            options (list[str]): The list of options to remove.
-        """
-        for option in options:
-            if option in self.additional_default_options:
-                self.additional_default_options.pop(option)
-            else:
-                self._DEFAULT_OPTIONS.pop(option)
