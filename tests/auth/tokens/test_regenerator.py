d21c3ee807606ed9806964bfac24ba1aa6b86aa2
Cesar E Garza <cesar@cegarza.com>
2023-10-07 22:50:17 -0500

    test_generate_bullet_token

--- tests/auth/tokens/test_regenerator.py
+++ tests/auth/tokens/test_regenerator.py
@@ -16,6 +16,8 @@ from splatnet3_scraper.constants import (
 )
 
 test_date_str = "2023-01-01 00:00:00"
+base_regen_path = "splatnet3_scraper.auth.tokens.regenerator"
+regen_path = base_regen_path + ".TokenRegenerator"
 
 
 class TestTokenRegenerator:
@@ -54,3 +56,43 @@ class TestTokenRegenerator:
             assert gtoken.value == "test_gtoken"
             assert gtoken.name == TOKENS.GTOKEN
             assert gtoken.timestamp == time.time()
+
+    @pytest.mark.parametrize(
+        "with_gtoken",
+        [True, False],
+        ids=["with_gtoken", "without_gtoken"],
+    )
+    @freezegun.freeze_time(test_date_str)
+    def test_generate_bullet_token(self, with_gtoken: bool) -> None:
+        nso = MagicMock()
+        if with_gtoken:
+            nso._user_info = {"test": "test"}
+            nso._gtoken = "test_gtoken"
+        else:
+            nso._user_info = None
+
+        test_gtoken = MagicMock()
+        test_gtoken.value = "test_gtoken"
+        with (
+            patch(regen_path + ".generate_gtoken") as mock_generate_gtoken,
+            patch(base_regen_path + ".Token") as mock_token,
+        ):
+            mock_generate_gtoken.return_value = test_gtoken
+            nso.get_bullet_token.return_value = "test_bullet_token"
+            TokenRegenerator.generate_bullet_token(nso, self.ftokens_url)
+            if with_gtoken:
+                mock_generate_gtoken.assert_not_called()
+                mock_token.assert_called_once_with(
+                    "test_bullet_token",
+                    TOKENS.BULLET_TOKEN,
+                    time.time(),
+                )
+            else:
+                mock_generate_gtoken.assert_called_once_with(
+                    nso, self.ftokens_url
+                )
+                mock_token.assert_called_once_with(
+                    "test_bullet_token",
+                    TOKENS.BULLET_TOKEN,
+                    time.time(),
+                )
