47006397b5f76dea6ae9b39532336ec96a6d7d1d
Cesar E Garza <cesar@cegarza.com>
2023-10-18 02:54:33 -0500

    test_from_file

--- tests/query/configuration/test_config.py
+++ tests/query/configuration/test_config.py
@@ -94,3 +94,76 @@ class TestConfig:
         else:
             assert config.get_value("test", default) == value
         mock_handler.get_value.assert_called_once_with("test")
+
+    @pytest.mark.parametrize(
+        "prefix",
+        [
+            "test",
+            None,
+        ],
+        ids=[
+            "prefix",
+            "no prefix",
+        ],
+    )
+    @pytest.mark.parametrize(
+        "save_to_file",
+        [
+            True,
+            False,
+        ],
+        ids=[
+            "save to file",
+            "no save to file",
+        ],
+    )
+    def test_from_file(
+        self,
+        save_to_file: bool,
+        prefix: str | None,
+    ) -> None:
+        mock_handler_instance = MagicMock()
+        mock_configp = MagicMock()
+        mock_config_instance = MagicMock()
+        mock_constructor = MagicMock()
+
+        expected_prefix = prefix or "SN3S"
+        expected_file_path = "test" if save_to_file else None
+
+        with (
+            patch(
+                base_config_path + ".configparser.ConfigParser"
+            ) as mock_configparser,
+            patch(base_config_path + ".ConfigOptionHandler") as mock_handler,
+            patch(config_path) as mock_config,
+            patch(base_config_path + ".TokenManagerConstructor") as mock_tmc,
+        ):
+            mock_configparser.return_value = mock_configp
+            mock_handler.return_value = mock_handler_instance
+            mock_config.return_value = mock_config_instance
+            mock_config.DEFAULT_PREFIX = "SN3S"
+            mock_tmc.from_tokens.return_value = mock_constructor
+
+            config = Config.from_file(
+                "test",
+                save_to_file=save_to_file,
+                prefix=prefix,
+            )
+            mock_configparser.assert_called_once_with()
+            mock_configp.read.assert_called_once_with("test")
+            mock_handler.assert_called_once_with(prefix=expected_prefix)
+            mock_handler_instance.read_from_configparser.assert_called_once_with(
+                mock_configp
+            )
+
+            assert mock_handler_instance.get_value.call_count == 3
+            mock_tmc.from_tokens.assert_called_once_with(
+                session_token=mock_handler_instance.get_value.return_value,
+                gtoken=mock_handler_instance.get_value.return_value,
+                bullet_token=mock_handler_instance.get_value.return_value,
+            )
+            mock_config.assert_called_once_with(
+                mock_handler_instance,
+                token_manager=mock_constructor,
+                output_file_path=expected_file_path,
+            )
