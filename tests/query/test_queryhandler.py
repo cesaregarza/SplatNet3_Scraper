94021ea8a82201875dbd7fb82f897bd3550538e0
Cesar E Garza <cesar@cegarza.com>
2023-10-21 13:55:19 -0500

    started reworking test_queryhandler

--- tests/query/test_queryhandler.py
+++ tests/query/test_queryhandler.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 
@@ -8,177 +8,21 @@ from splatnet3_scraper.query.handler import QueryHandler
 from splatnet3_scraper.query.responses import QueryResponse
 from tests.mock import MockConfig, MockNSO, MockResponse, MockTokenManager
 
-config_path = "splatnet3_scraper.query.config.Config"
-query_response_path = "splatnet3_scraper.query.responses.QueryResponse"
-nso_path = "splatnet3_scraper.auth.nso.NSO"
-token_manager_path = "splatnet3_scraper.auth.token_manager.TokenManager"
-query_handler_path = "splatnet3_scraper.query.handler.QueryHandler"
+base_handler_path = "splatnet3_scraper.query.handler"
+config_path = base_handler_path + ".Config"
+handler_path = base_handler_path + ".QueryHandler"
 
 
 class TestSplatNetQueryHandler:
-    def test_from_config_file(self, monkeypatch: pytest.MonkeyPatch):
+    def test_init(self) -> None:
+        config = MagicMock()
+        handler = QueryHandler(config)
+        assert handler.config == config
 
-        # No config file
-        with (
-            monkeypatch.context() as m,
-            patch(config_path + ".__init__") as mock_config,
-        ):
-            m.setattr(QueryHandler, "__init__", lambda x, y: None)
-            mock_config.return_value = None
-            QueryHandler.from_config_file()
-            mock_config.assert_called_once()
-
-        # Config file
-        with (
-            monkeypatch.context() as m,
-            patch(config_path + ".__init__") as mock_config,
-        ):
-            m.setattr(QueryHandler, "__init__", lambda x, y: None)
-            mock_config.return_value = None
-            QueryHandler.from_config_file("test_config_path")
-            mock_config.assert_called_once_with("test_config_path")
-
-    def test_generate_session_token_url(self, monkeypatch: pytest.MonkeyPatch):
-        monkeypatch.setattr(NSO, "new_instance", MockNSO.new_instance)
-
-        (
-            url,
-            state,
-            verifier,
-        ) = QueryHandler.generate_session_token_url()
-        assert url == "test_url"
-        assert state == b"test_state"
-        assert verifier == b"test_verifier"
-
-    def test_from_session_token(self):
-        with (
-            patch(
-                token_manager_path + ".from_session_token"
-            ) as mock_from_token,
-            patch.object(
-                MockTokenManager, "generate_all_tokens"
-            ) as mock_generate,
-            patch(config_path + ".__init__") as mock_config,
-        ):
-            mock_from_token.return_value = MockTokenManager()
-            mock_config.return_value = None
-            QueryHandler.from_session_token("test_token")
-            mock_from_token.assert_called_once_with("test_token")
-            mock_generate.assert_called_once()
-
-    def test_from_tokens(self):
-        from_tokens_path = token_manager_path + ".from_tokens"
-        with (patch(from_tokens_path) as mock_from_tokens,):
-            mock_from_tokens.return_value = MockTokenManager()
-            QueryHandler.from_tokens("test_session_token", "test_token")
-            mock_from_tokens.assert_called_once_with(
-                "test_session_token", "test_token", None
-            )
-
-    def test_from_env(self):
-        with patch(config_path + ".from_env") as mock_from_env:
-            mock_from_env.return_value = MockConfig()
-            QueryHandler.from_env()
-            mock_from_env.assert_called_once()
-
-    def test_query(self):
-        scraper = QueryHandler(MockConfig())
-        mock_json = {"data": "test_data"}
-        # 200 response
-        with (
-            patch.object(scraper, "_QueryHandler__query") as mock_get,
-            patch.object(
-                MockTokenManager, "generate_all_tokens"
-            ) as mock_generate,
-        ):
-            mock_get.return_value = MockResponse(200, json=mock_json)
-            assert scraper.query("test_query") == QueryResponse(
-                mock_json["data"]
-            )
-            mock_get.assert_called_once_with("test_query", {})
-            mock_generate.assert_not_called()
-
-        # 200 response, with errors
-        with (
-            patch.object(scraper, "_QueryHandler__query") as mock_get,
-            patch.object(
-                MockTokenManager, "generate_all_tokens"
-            ) as mock_generate,
-        ):
-            mock_get.return_value = MockResponse(
-                200, json={"errors": "test_error"}
-            )
-            with pytest.raises(SplatNetException):
-                scraper.query("test_query")
-            mock_get.assert_called_once_with("test_query", {})
-            mock_generate.assert_not_called()
-
-        # Not 200 response
-        with (
-            patch.object(scraper, "_QueryHandler__query") as mock_get,
-            patch.object(
-                MockTokenManager, "generate_all_tokens"
-            ) as mock_generate,
-        ):
-            mock_get.return_value = MockResponse(404, json=mock_json)
-            assert scraper.query("test_query") == QueryResponse(
-                mock_json["data"]
-            )
-            assert mock_get.call_count == 2
-            mock_generate.assert_called_once()
-
-    def test_query_hash(self):
-        scraper = QueryHandler(MockConfig())
-        mock_json = {"data": "test_data"}
-        # 200 response
-        with (
-            patch.object(scraper, "_QueryHandler__query_hash") as mock_get,
-            patch.object(
-                MockTokenManager, "generate_all_tokens"
-            ) as mock_generate,
-        ):
-            mock_get.return_value = MockResponse(200, json=mock_json)
-            assert scraper.query_hash("test_query") == QueryResponse(
-                mock_json["data"]
-            )
-            mock_get.assert_called_once_with("test_query", {})
-            mock_generate.assert_not_called()
-
-        # 200 response, with errors
-        with (
-            patch.object(scraper, "_QueryHandler__query_hash") as mock_get,
-            patch.object(
-                MockTokenManager, "generate_all_tokens"
-            ) as mock_generate,
-        ):
-            mock_get.return_value = MockResponse(
-                200, json={"errors": "test_error"}
-            )
-            with pytest.raises(SplatNetException):
-                scraper.query_hash("test_query")
-            mock_get.assert_called_once_with("test_query", {})
-            mock_generate.assert_not_called()
-
-        # Not 200 response
-        with (
-            patch.object(scraper, "_QueryHandler__query_hash") as mock_get,
-            patch.object(
-                MockTokenManager, "generate_all_tokens"
-            ) as mock_generate,
-        ):
-            mock_get.return_value = MockResponse(404, json=mock_json)
-            assert scraper.query_hash("test_query") == QueryResponse(
-                mock_json["data"]
-            )
-            assert mock_get.call_count == 2
-            mock_generate.assert_called_once()
-
-    def test_export_tokens(self):
-        with (
-            patch.object(MockTokenManager, "export_tokens") as mock_export,
-            patch(config_path + ".__init__") as mock_config,
-        ):
-            mock_config.return_value = None
-            scraper = QueryHandler(MockConfig())
-            scraper.export_tokens()
-            mock_export.assert_called_once()
+    def test_from_config_file(self) -> None:
+        config = MagicMock()
+        with patch(config_path) as mock_config:
+            mock_config.from_file.return_value = config
+            handler = QueryHandler.from_config_file("test")
+            mock_config.from_file.assert_called_once_with("test")
+            assert handler.config == config
